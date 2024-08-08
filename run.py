c5bfeda5967c4a0974dfdd8f3fea105a07edeff7
“Riya <riyaanarayan@gmail.com>
2024-08-08 23:36:11 +0530

    "Added Swagger Documentation"

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
+from flask import Flask
+from flasgger import Swagger
 from app import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
+swagger = Swagger(app)
+
+if __name__ == "__main__":
     app.run(debug=True)
